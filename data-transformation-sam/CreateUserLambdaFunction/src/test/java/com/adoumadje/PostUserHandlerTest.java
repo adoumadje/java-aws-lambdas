09cb3b0d42992f8214590af225b832e811ce0086
adoumadje <adoumadjecaleb@gmail.com>
2025-10-28 07:28:35 +0100

    data transformation - starter

---
+++ data-transformation-sam/CreateUserLambdaFunction/src/test/java/com/adoumadje/PostUserHandlerTest.java
@@ -0,0 +1,22 @@
+package com.adoumadje;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PostUserHandlerTest {
+  @Test
+  public void successfulResponse() {
+    PostUserHandler postUserHandler = new PostUserHandler();
+    APIGatewayProxyResponseEvent result = postUserHandler.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
