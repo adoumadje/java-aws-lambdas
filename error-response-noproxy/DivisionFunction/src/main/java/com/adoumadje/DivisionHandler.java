6fee4af9844dfb903dfeb20bf24c71f0cdf44fa4
adoumadje <adoumadjecaleb@gmail.com>
2025-10-29 10:36:38 +0100

    error response no proxy - config

--- error-response-noproxy/DivisionFunction/src/main/java/com/adoumadje/DivisionHandler.java
+++ error-response-noproxy/DivisionFunction/src/main/java/com/adoumadje/DivisionHandler.java
@@ -4,6 +4,7 @@ import com.adoumadje.domain.Division;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,6 +13,18 @@ import java.util.Map;
 public class DivisionHandler implements RequestHandler<Map<String, String>, Division> {
 
     public Division handleRequest(final Map<String, String> input, final Context context) {
-        return null;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        int dividend = Integer.parseInt(input.get("dividend"));
+        int divisor = Integer.parseInt(input.get("divisor"));
+        int result = dividend / divisor;
+
+        Division division = new Division();
+        division.setDividend(dividend);
+        division.setDivisor(divisor);
+        division.setResult(result);
+
+        return division;
     }
 }
