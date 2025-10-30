5b250171448fa15158bed7a2f10706cae5852732
adoumadje <adoumadjecaleb@gmail.com>
2025-10-30 11:49:56 +0100

    cognito auth app - starter

---
+++ cognito-auth-app/CognitoAuthApp/src/main/java/com/adoumadje/cognito/auth/app/controller/SignUpHandler.java
@@ -0,0 +1,24 @@
+package com.adoumadje.cognito.auth.app.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class SignUpHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        return null;
+    }
+}
